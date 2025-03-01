d61b2633e4f5e65ba9ecc36af3c1624e6ddd0f24
Marcus Davi <davimarcus96@gmail.com>
2025-02-28 21:14:42 -0300

    correction of misspelled words

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const inputModal = document.createElement("dialog");
 document.body.appendChild(inputModal);
 
-const newDrawingBtn = document.querySelector("div#novo-desenho");
+const newDrawingBtn = document.querySelector("div#new-drawing");
 const drawings = document.querySelector("div#drawings");
 const header = document.querySelector("header");
-const searchBtn = header.querySelector("#pesquisarBtn");
-const searchInput = header.querySelector("input#pesquisarQuery");
-const more = header.querySelector("#mais");
+const searchBtn = header.querySelector("#searchBtn");
+const searchInput = header.querySelector("input#searchQuery");
+const more = header.querySelector("#more");
 const configBtn = document.querySelector("#configBtn");
-const closeConfigMenuBtn = document.querySelector("#sair-menu-config");
-const configMenu = document.querySelector("#menu-configuracao");
+const closeConfigMenuBtn = document.querySelector("#close-config-menu");
+const configMenu = document.querySelector("#config-menu");
 
 const aside = document.querySelector("aside");
 let asideClosed = true;
@@ -39,12 +39,12 @@ sharedWorker.port.onmessage = (ev) => {
             // atualiza o contador de desenhos da seção em que o desenho foi criado.
             asideSections[["tudo", "favoritados", "arquivados"].indexOf(asideSelectedSection)].children[1].textContent = +asideSections[["tudo", "favoritados", "arquivados"].indexOf(asideSelectedSection)].children[1].textContent + 1;
         } else {
-            const aviso = document.createElement("span");
-            aviso.innerHTML = `<br> ${msg.mensagemErro} <br>`;
-            aviso.id = "aviso-input-invalido";
-            aviso.setAttribute("role", "alert");
-            aviso.style.color = "red";
-            inputModal.insertBefore(aviso, inputModal.querySelector("div"));
+            const advice = document.createElement("span");
+            advice.innerHTML = `<br> ${msg.mensagemErro} <br>`;
+            advice.id = "aviso-input-invalido";
+            advice.setAttribute("role", "alert");
+            advice.style.color = "red";
+            inputModal.insertBefore(advice, inputModal.querySelector("div"));
         }
     }
     else if (msg.type === "delete drawing") {
@@ -69,14 +69,14 @@ sharedWorker.port.onmessage = (ev) => {
         if (msg.result === "success" && msg.section === "tudo" && msg.favoritated) {
             for (let i = 0; i < drawings.children.length; i++) {
                 if (drawings.children[i].querySelector(".info").children[0].textContent === msg.name) {
-                    drawings.children[i].querySelector(".opcoes").innerHTML = `<svg viewBox='0 0 576 512' height='17.7px' width='19.7px'><path fill='gold' d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z'/></svg> ${drawings[i].children[1].children[1].innerHTML}`;
+                    drawings.children[i].querySelector(".options").innerHTML = `<svg viewBox='0 0 576 512' height='17.7px' width='19.7px'><path fill='gold' d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z'/></svg> ${drawings[i].children[1].children[1].innerHTML}`;
                     drawings.children[i].querySelector("[popover]").children[2].textContent = "Desfavoritar";
                 }
             }
         } else if (msg.result === "success" && msg.section === "tudo" && !msg.favoritated) {
             for (let i = 0; i < drawings.children.length; i++) {
                 if (drawings.children[i].querySelector(".info").children[0].textContent === msg.name) {
-                    drawings.children[i].querySelector(".opcoes").children[0].remove();
+                    drawings.children[i].querySelector(".options").children[0].remove();
                     drawings.children[i].querySelector("[popover]").children[2].textContent = "Favoritar";
                 }
             }
@@ -258,7 +258,7 @@ sharedWorker.port.onmessage = (ev) => {
 */
 function renderDrawing(infos) {
     const drawing = document.createElement("div");
-    drawing.classList.add("desenho");
+    drawing.classList.add("drawing");
 
     const img = document.createElement("img");
     img.addEventListener('click', function(){
@@ -277,11 +277,11 @@ function renderDrawing(infos) {
     info.classList.add("info");
     info.innerHTML = `<span>${infos.name}</span>`;
 
-    const opcoes = document.createElement("div");
-    opcoes.classList.add("opcoes");
-    opcoes.innerHTML = "<button><svg viewBox='0 0 448 512' height='17.7px' width='19.7px'><path fill='red' d='M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z'/></svg></button>  <button><svg viewBox='0 0 128 512' width='10px' height='19.7px'><path fill='currentColor' d='M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z'/></svg></button>";
+    const options = document.createElement("div");
+    options.classList.add("options");
+    options.innerHTML = "<button><svg viewBox='0 0 448 512' height='17.7px' width='19.7px'><path fill='red' d='M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z'/></svg></button>  <button><svg viewBox='0 0 128 512' width='10px' height='19.7px'><path fill='currentColor' d='M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z'/></svg></button>";
     if (infos.favoritated) {
-        opcoes.innerHTML = `<svg viewBox='0 0 576 512' height='17.7px' width='19.7px'><path fill='gold' d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z'/></svg> ${opcoes.innerHTML}`;
+        options.innerHTML = `<svg viewBox='0 0 576 512' height='17.7px' width='19.7px'><path fill='gold' d='M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z'/></svg> ${opcoes.innerHTML}`;
     }
 
     const extraOptions = document.createElement("div");
@@ -319,13 +319,13 @@ function renderDrawing(infos) {
     });
 
     // primeira opção
-    opcoes.querySelectorAll("button")[0].addEventListener('click', function () {
+    options.querySelectorAll("button")[0].addEventListener('click', function () {
         sharedWorker.port.postMessage({ type: "delete drawing", name: infos.name, section: asideSelectedSection });
     })
 
     drawing.appendChild(img);
     drawing.appendChild(info);
-    info.appendChild(opcoes);
+    info.appendChild(options);
     drawing.appendChild(extraOptions);
 
     drawings.appendChild(drawing);
@@ -335,12 +335,12 @@ function renderDrawing(infos) {
     }, 500);
 
     // garante que não vão existir dois do mesmo id para o container de mais opções.
-    for (let i = 0; i < document.querySelectorAll("div#drawings > div.desenho").length; i++) {
-        const maisOpcoesContainer = document.querySelectorAll("div#drawings > div.desenho")[i].querySelector("div[popover]");
-        maisOpcoesContainer.id = `mais-opcoes-${i + 1}`;
+    for (let i = 0; i < document.querySelectorAll("div#drawings > div.drawing").length; i++) {
+        const moreOptionsContainer = document.querySelectorAll("div#drawings > div.drawing")[i].querySelector("div[popover]");
+        moreOptionsContainer.id = `more-options-${i + 1}`;
 
-        const maisOpcoesBtn = document.querySelectorAll("div#drawings > div.desenho > div.info")[i].querySelectorAll("button")[1];
-        maisOpcoesBtn.setAttribute('popovertarget', maisOpcoesContainer.id);
+        const moreOptionsBtn = document.querySelectorAll("div#drawings > div.drawing > div.info")[i].querySelectorAll("button")[1];
+        moreOptionsBtn.setAttribute('popovertarget', moreOptionsContainer.id);
     }
     return drawing;
 }
@@ -461,7 +461,7 @@ function esconderMenuConfiguracao() {
 }
 
 function updateDrawingsMenuPosition() {
-    for (const menu of document.querySelectorAll("div.desenho > div[popover]")) {
+    for (const menu of document.querySelectorAll("div.drawing > div[popover]")) {
         menu.style.marginLeft = `${menu.parentElement.getBoundingClientRect().x + menu.parentElement.getBoundingClientRect().width}px`;
         menu.style.marginTop = `${menu.parentElement.getBoundingClientRect().y + 10}px`;
     }
